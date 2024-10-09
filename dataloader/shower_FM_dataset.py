4076a50aadfc968e9b6381ba5aecfdc2474976fa
Kyongmin Yeo <kyeo@us.ibm.com>
2024-10-09 10:44:48 -0400

    added FM dataset

--- dataloader/shower_FM_dataset.py
+++ dataloader/shower_FM_dataset.py
@@ -69,6 +69,14 @@ CONSTANTS.N_CELLS_Z = CONSTANTS.Z_HIGH - CONSTANTS.Z_LOW + 1
 CONSTANTS.MAX_ENERGY = 1000 # changed to 1 TeV
 CONSTANTS.MAX_ANGLE  = 70
 
+#Define Geometry coding
+def geo_coding(x):
+    geo =  ['FCCeeALLEGRO','FCCeeCLD','ODD','Par04_PbWO4','Par04_SciPb','Par04_SiW']
+    out =  [1 if i == geo.index(x) else 0 for i in range(len(geo))]
+    return out
+
+CONSTANTS.GEO_CODING = geo_coding
+
 #############################################
 # Copy the global parameters
 #############################################
@@ -89,121 +97,105 @@ N_CELLS_Z = CONSTANTS.N_CELLS_Z
 MAX_ENERGY = CONSTANTS.MAX_ENERGY
 MAX_ANGLE  = CONSTANTS.MAX_ANGLE 
 
+GEO_CODING = CONSTANTS.GEO_CODING
+
 #data loader
 class Dataset(Dataset):
     def __init__(
         self,
-        data_split   = [0.8,0.1,0.1],
-        root_path    = '.',
-        valid_path   = None,
-        geo          = ['SiW'],
-        use_cond_info= True,
+        data_path=['./'],
+        geo = [''],
         split="train",
         scale_method = None,
         max_num_events = None,
         max_local_data = 100000,
+        num_samples_epoch = None,
+        random_geo_sampling = False,
         **kwargs
     ):
-        assert split in ["train", "test", "val"]
-        type_map = {"train": 0, "val": 1, "test": 2}
-        self.set_type = type_map[split]
 
-        self.data_split   = data_split
-        self.root_path = root_path
+        self.split = split
 
-        if valid_path != None and self.set_type == 0:
-            self.data_split = [self.data_split[0]+self.data_split[1], 0, self.data_split[2]]
-        elif valid_path != None and self.set_type == 1:
-            self.data_split = [0, 1.0, 0]
-            self.root_path = valid_path
+        self.data_files = {}
+        for k in range(len(geo)):
+            self.data_files[geo[k]] = data_path[k]
 
-
-        self.geo      = geo
-
-        self.use_cond_info = use_cond_info
         self.scale_method = scale_method
         self.max_num_events = max_num_events
         self.max_local_data = max_local_data
+
+        self.num_samples_epoch = num_samples_epoch
+
+        self.random_geo_sampling = random_geo_sampling
     
         self.__read_data__()
 
     def __read_data__(self):
-        energies_data = []
-        cond_e_data = []
-        cond_phi_train = []
-        cond_theta_train = []
-        cond_geo_data = []
+
+        geo_data = []
+
+        for geo,path in self.data_files.items():
+
+            energies_data = []
+            cond_e_data = []
+            cond_phi_train = []
+            cond_theta_train = []
+            cond_geo_data = []
             
-        max_local_data = self.max_local_data
+            max_local_data = self.max_local_data
 
-        #get all the file names
-        file_list = []
-        for file in os.listdir(self.root_path):
-            if file.endswith(".h5"):
-                file_list.append(file)
+            #get all the file names
+            file_list = []
+            for file in os.listdir(path):
+                if file.endswith(".h5"):
+                    file_list.append(file)
 
-        #First check the total file size
-        total_num_events = 0
-        local_events = []
+            #First check the total file size
+            total_num_events = 0
+            local_num_events = []
 
-        dir_geo = self.root_path + "/"
-        for f_name in file_list:
-            F_name = dir_geo+f_name
-            with h5py.File(F_name, 'r') as hdf5_file:
-                h5 = hdf5_file['showers']
+            for f_name in file_list:
+                F_name = path + '/' +f_name
+                with h5py.File(F_name, 'r') as hdf5_file:
+                    h5 = hdf5_file['showers']
 
-                h5_events = h5.shape[0]
-                if self.max_num_events == None:
-                    num_events = h5_events
-                else:
-                    num_events = min(self.max_num_events,h5_events)
-                print(f'{num_events} number of events in {f_name}')
+                    h5_events = h5.shape[0]
+                    if self.max_num_events == None:
+                        num_events = h5_events
+                    else:
+                        num_events = min(self.max_num_events,h5_events)
+                    print(f'{num_events} number of events in {f_name}')
 
-                #data split
-                ntrain = int(self.data_split[0]*num_events)
-                nvalid = int(self.data_split[1]*num_events)
+                    total_num_events +=  num_events
+                    local_num_events += [num_events]
 
-                data_bd = [0,ntrain,ntrain+nvalid,num_events]
+            #Pre-allocate the array
+            energies_data = np.zeros((total_num_events,N_CELLS_R,N_CELLS_PHI,N_CELLS_Z),dtype=np.float32)
+            cond_e_data = np.zeros((total_num_events,),dtype=np.float32)
+            theta_data = np.zeros((total_num_events,),dtype=np.float32)
+            phi_data = np.zeros((total_num_events, 2),dtype=np.float32) # each phi is processed into 2 values
 
-                local_events.append([data_bd[self.set_type],data_bd[self.set_type+1]])
-                total_num_events += local_events[-1][1] - local_events[-1][0]
+            file_idx  = 0
+            start_idx = 0
 
-        #Pre-allocate the array
-        energies_data = np.zeros((total_num_events,N_CELLS_R,N_CELLS_PHI,N_CELLS_Z),dtype=np.float32)
-        cond_e_data = np.zeros((total_num_events,),dtype=np.float32)
-        theta_data = np.zeros((total_num_events,),dtype=np.float32)
-        phi_data = np.zeros((total_num_events, 2),dtype=np.float32) # each phi is processed into 2 values
+            for f_name in file_list:
+                F_name = path + '/' +f_name
 
-        file_idx  = 0
-        start_idx = 0
+                local_num_data = local_num_events[file_idx]
+                file_idx += 1
 
-        dir_geo = self.root_path + "/"
-        for f_name in file_list:
-            F_name = dir_geo+f_name
-
-            local_id0 = local_events[file_idx][0]
-            local_id1 = local_events[file_idx][1]
-            local_num_data = local_id1-local_id0
-
-            file_idx += 1
-
-            print(f_name)
-            #print(start_idx)
-            #print(local_id0,local_id1)
-            # print(local_num_data)
-            angle_particle = 70
-            geo = "SiW"
+                print(f_name)
             
-            t0 = time.time()
-            with h5py.File(F_name, 'r') as hdf5_file:
-                h5_showers = hdf5_file['showers']
-                h5_energy_particle = hdf5_file['incident_energy']
-                h5_theta = hdf5_file['incident_theta']
-                h5_phi = hdf5_file['incident_phi']
+                t0 = time.time()
+                with h5py.File(F_name, 'r') as hdf5_file:
+                    h5_phi =             hdf5_file['incident_phi']
+                    h5_theta =           hdf5_file['incident_theta']
+                    h5_showers =         hdf5_file['showers']
+                    h5_energy_particle = hdf5_file['incident_energy']
 
                 # events in Num_data x Radial x Azimuthal x Vertical
-                if local_num_data > max_local_data:
-                    id0 = local_id0
+                if local_num_data > max_local_data:  #incremental data loading if data size is too big
+                    id0 = 0
                     for k in range(int(local_num_data/max_local_data)):
                         
                         id1 = id0 + max_local_data
@@ -220,11 +212,13 @@ class Dataset(Dataset):
                         start_idx = end_idx
                         #print(end_idx)
                         
-                    if id0 < local_id1:
+                    if id0 < local_num_data:
 
-                        local_energy, local_energy_particle, local_theta, local_phi = self.read_local(id0, local_id1, h5_showers, h5_energy_particle, h5_theta, h5_phi)
+                        id1 = local_num_data
 
-                        end_idx = start_idx + local_id1-id0
+                        local_energy, local_energy_particle, local_theta, local_phi = self.read_local(id0, id1, h5_showers, h5_energy_particle, h5_theta, h5_phi)
+
+                        end_idx = start_idx + (local_num_data-id0)
                         energies_data[start_idx:end_idx] = local_energy
                         cond_e_data[start_idx:end_idx] = local_energy_particle
                         theta_data[start_idx:end_idx] = local_theta
@@ -233,7 +227,7 @@ class Dataset(Dataset):
                         #print(end_idx)
                 else:
 
-                    local_energy, local_energy_particle, local_theta, local_phi = self.read_local(local_id0, local_id1, h5_showers, h5_energy_particle, h5_theta, h5_phi)
+                    local_energy, local_energy_particle, local_theta, local_phi = self.read_local(0, local_num_data, h5_showers, h5_energy_particle, h5_theta, h5_phi)
 
                     end_idx = start_idx + local_num_data
                     energies_data[start_idx:end_idx] = local_energy
@@ -242,38 +236,47 @@ class Dataset(Dataset):
                     phi_data[start_idx:end_idx] = local_phi
                     start_idx = end_idx
                     
-            #print('data retrieval time:',time.time()-t0)
+                #print('data retrieval time:',time.time()-t0)
 
-            # build the geometry condition vector (1 hot encoding vector)
-            if geo == "SiW":
-                cond_geo_data.append([[0, 1]] * local_num_data)
-            if geo == "SciPb":
-                cond_geo_data.append([[1, 0]] * local_num_data)
+                # build the geometry condition vector (1 hot encoding vector)
+                cond_geo_data.append([GEO_CODING(geo)]*local_num_data)
 
-        cond_geo_data   = np.concatenate(cond_geo_data)
+            cond_geo_data   = np.concatenate(cond_geo_data)
 
-        self.data_energy = torch.from_numpy(energies_data)
-        self.data_cond_e = torch.from_numpy(cond_e_data)
-        self.data_cond_theta = torch.from_numpy(theta_data)
-        self.data_cond_phi = torch.from_numpy(phi_data)
-        self.data_cond_g = torch.from_numpy(cond_geo_data)
+            data = {}
+            data['shower'] = torch.from_numpy(energies_data)
+            data['energy'] = torch.from_numpy(cond_e_data)
+            data['theta']  = torch.from_numpy(theta_data)
+            data['phi']    = torch.from_numpy(phi_data)
+            data['geo']    = torch.from_numpy(cond_geo_data)
 
+            geo_data += [data]
+
+        self.geo_data = geo_data
 
     def __getitem__(self, index):
 
-        if self.use_cond_info:
-            return self._torch(self.data_energy[index],
-                               self.data_cond_e[index:index+1],
-                               self.data_cond_theta[index:index+1],
-                               self.data_cond_phi[index],
-                               self.data_cond_g[index]),        \
-                   self._torch(self.data_energy[index])[0]
+        if self.random_geo_sampling:
+            geo_id = torch.randint(len(self.geo_data),(1,)).item()
         else:
-            return self._torch(self.data_energy[index]),        \
-                   self._torch(self.data_energy[index])[0]
+            geo_id = index % len(self.geo_data)
+
+        out_data = self.geo_data[geo_id]
+
+        return self._torch(out_data['shower'][index],
+                           out_data['energy'][index:index+1],
+                           out_data['theta' ][index:index+1],
+                           out_data['phi'   ][index],
+                           out_data['geo'   ][index]),        \
+               self._torch(out_data['shower'][index])[0]
 
     def __len__(self):
-        return len(self.data_energy)
+        if self.num_samples_epoch == None:
+            n_samples = 0
+            for i in range(len(self.geo_data)):
+                self.num_samples_epoch = max(n_samples,self.geo_data[i]['shower'].size(0))
+            print(f'number of samples per epoch is {self.num_samples_epoch}')
+        return self.num_samples_epoch
 
     def _torch(self,*dfs):
         if torch.is_tensor(dfs[0]):
